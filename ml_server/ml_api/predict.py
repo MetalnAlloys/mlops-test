d0e834f0550fbbf2ed12a4e786ad3f483350256b
MetalnAlloys <ali.scmenust@gmail.com>
2025-05-10 13:09:12 +0200

    use labels instead of loading iris dataset

--- ml_server/ml_api/predict.py
+++ ml_server/ml_api/predict.py
@@ -2,20 +2,18 @@ import joblib
 import pandas as pd
 import os
 
-from sklearn.datasets import load_iris
+from django.conf import settings
 
-iris = load_iris(as_frame=True)
-target_names = iris.target_names  # ['setosa' 'versicolor' 'virginica']
+ARTIFACTS_DIR = str(os.path.join(settings.BASE_DIR) + "/artifacts")
+LABELS = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
 
 
 class CreateLogisticRegression:
     def __init__(self):
-        root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        self.pipeline = joblib.load(os.path.join(root, "artifacts", "model_pipeline.joblib"))
+        self.pipeline = joblib.load(os.path.join(ARTIFACTS_DIR, "model_pipeline.joblib"))
 
     def preprocessing(self, input_data):
-        data_array = pd.DataFrame(input_data, index=[0]).to_numpy()
-        return pd.DataFrame(data_array, columns=iris.feature_names)
+        return pd.DataFrame(input_data, index=[0]).to_numpy()
 
     def compute_prediction(self, input_data):
         try:
@@ -27,11 +25,11 @@ class CreateLogisticRegression:
         return prediction
 
     def predict(self, input_data):
-        prediction = self.pipeline.predict(input_data)[0]
+        prediction = self.pipeline.predict(input_data)
 
         return {
-            "prediction": prediction, 
-            "prediction_label": target_names[prediction]
+            "prediction": prediction[0], 
+            "prediction_label": LABELS[prediction[0]]
         }
 
 
