67c88db23f4a9c1a1d944a2e6b550735dd7719d6
MetalnAlloys <ali.scmenust@gmail.com>
2025-05-10 13:19:49 +0200

    simple unit tests

--- ml_server/ml_api/tests.py
+++ ml_server/ml_api/tests.py
@@ -1,10 +1,10 @@
-import inspect
-from .registry import Registry
 from django.test import TestCase
+
+from .registry import Registry
 from .predict import CreateLogisticRegression
 
-class MLTests(TestCase):
-    def test_algorithm(self):
+class MLOpsTests(TestCase):
+    def test_logistic_regression_algorithm(self):
         input_data = {
          'sepal_length': 5.1,
          'sepal_width': 3.5,
@@ -12,25 +12,23 @@ class MLTests(TestCase):
          'petal_width': 0.2
         }
 
-        alg = CreateLogisticRegression()
-        response = alg.compute_prediction(input_data)
+        test_algorithm = CreateLogisticRegression()
+        response = test_algorithm.compute_prediction(input_data)
         self.assertTrue('prediction_label' in response)
         self.assertEqual('setosa', response['prediction_label'])
 
 
-    def test_registry(self):
+    def test_adding_new_algorithm(self):
         registry = Registry()
         self.assertEqual(len(registry.endpoints), 0)
-        endpoint_name = "lr"
+
+        endpoint_name = "lar"
         algorithm_object = CreateLogisticRegression()
         algorithm_name = "Logistic regression"
         algorithm_version = "0.0.1"
 
-        # Test adding an algorithm to registry
-        registry.add_algorithm(endpoint_name,
-                               algorithm_object,
-                               algorithm_name,
-                               algorithm_version)
+        # Test adding an algorithm to the registry
+        registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name, algorithm_version)
         self.assertEqual(len(registry.endpoints), 1)
 
 
